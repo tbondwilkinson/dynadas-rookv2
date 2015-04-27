a5ca233fbd8967762d60477543ef8e3954c97121
tbondwilkinson <tbondwilkinson@gmail.com>
2015-04-27 16:05:45 -0500

    Broken dependencies

--- init.r
+++ init.r
@@ -1,3 +1,3 @@
-install.packages('sfsmisc', dependencies = TRUE)
-install.packages('ltm', dependencies = TRUE)
+#install.packages('sfsmisc', dependencies = TRUE)
+#install.packages('ltm', dependencies = TRUE)
 install.packages('./CATPack_0.1.tar.gz', dependencies=TRUE, repos=NULL, type="source")
