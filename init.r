966bc624b28cf23a3a9f433f963ef42cc8b86049
tbondwilkinson <tbondwilkinson@gmail.com>
2015-04-28 12:59:54 -0500

    Fixing

--- init.r
+++ init.r
@@ -1,1 +1,2 @@
-#install.packages('./CATPack_0.1.tar.gz', repos=NULL, type="source")
+.libPaths( c( "./.apt/usr/lib/R/site-library", .libPaths() ) )
+install.packages('./CATPack_0.1.tar.gz', repos=NULL, type="source")
