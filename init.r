a575d063f58e3d24734072006d9b2277a58104ab
tbondwilkinson <tbondwilkinson@gmail.com>
2015-04-28 02:15:11 -0500

    fixing

---
+++ init.r
@@ -0,0 +1,4 @@
+install.packages('Rcmdr', dependences = TRUE)
+install.packages(c('sfsmisc'), dependencies = TRUE)
+install.packages(c('ltm'), dependencies = TRUE)
+install.packages('./CATPack_0.1.tar.gz', repos=NULL, type="source")
