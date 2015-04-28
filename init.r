2b41ece302916dce8c9a97c3a639183959911601
tbondwilkinson <tbondwilkinson@gmail.com>
2015-04-28 18:12:45 -0500

    Fixing

--- init.r
+++ init.r
@@ -1,2 +1,23 @@
-.libPaths( c( .libPaths() , "./.apt/usr/lib/R/site-library") )
+#
+# Example R code to install packages
+# See http://cran.r-project.org/doc/manuals/R-admin.html#Installing-packages for details
+#
+
+###########################################################
+# Update this line with the R packages to install:
+
+my_packages = c("sfsmisc", "ltm")
+
+###########################################################
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
+
 install.packages('./CATPack_0.1.tar.gz', repos=NULL, type="source")
