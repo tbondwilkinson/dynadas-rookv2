34947f6cf2a2bcf1d6b9c40b130b2e56d7b74e54
tom-sparo <tom@sparolabs.com>
2015-06-28 12:22:20 -0500

    Adding lookAheadEPVcpp

--- application.rb
+++ application.rb
@@ -60,4 +60,33 @@ EOF
   epvs = r.eval("epvs").to_ruby
   JSONP Hash["next_item" => next_item, "epvs" => epvs]
 
+post '/look_ahead' do
+  request.body.rewind
+  data = JSON.parse request.body.read
+  cat = data["cat"]
+  this_item = data["this_item"]
+
+  r = Rserve::Connection.new
+  r.void_eval <<-EOF
+cat <- new("CATsurv")
+json_cat <- fromJSON('#{cat.to_json}')
+cat@guessing <- json_cat$guessing
+cat@discrimination <- unlist(json_cat$discrimination)
+cat@answers <- as.numeric(json_cat$answers)
+cat@priorName <- json_cat$priorName
+cat@priorParams <- json_cat$priorParams
+cat@lowerBound <- json_cat$lowerBound
+cat@upperBound <- json_cat$upperBound
+cat@quadPoints <- json_cat$quadPoints
+cat@D <- json_cat$D
+cat@difficulty <- lapply(json_cat$difficulty, unlist)
+cat@X <- json_cat$X
+cat@poly <- TRUE
+response <- lookAheadEPVcpp(cat, #{this_item})
+next_items = response$next.items
+EOF
+  
+  next_items = r.eval("next_items").to_ruby
+  JSONP Hash["next_items" => next_items]
+
 end
